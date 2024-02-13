c75429acd6e99e612c2e51e8eb8b332f21524032
moon_coder <69103662+alisadeqi-py@users.noreply.github.com>
2024-02-13 08:28:08 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def load_data(data):
     for row in range(len(data.iloc[:, 0])):
         item = [data.iloc[row, 2], data.index[data[2] == data.iloc[row, 2]].tolist()[0]]
         url_list.append(item)
-        print('links collected')
     return url_list
 
 
@@ -20,18 +19,21 @@ def get_website_provider():
     data = pd.read_excel('Irvine-CA.xlsx')
     url_list = load_data(data)
     data['website_provider'] = None
-    for url in url_list:
+    for url, index in url_list:
 
         try:
-            response = requests.get(url[0])
+            print(url, index)
+            response = requests.get(url, verify=False)
+            print(response.status_code)
             if response.status_code == 200:
                 for domain in domains:
                     if domain in response.text:
-                        data.at[url[1], -1] = domain
+                        data.at[index, -1] = domain
+                        print(f'{domain} ,set')
             else:
-                return "Error: Status Code {}".format(response.status_code)
+                print("Error: Status Code {}".format(response.status_code))
         except Exception as e:
-            return "Error: {}".format(str(e))
+            print(f"Error:{e}")
 
 
-load_data()
+get_website_provider()
